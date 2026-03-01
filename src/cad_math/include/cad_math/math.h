72d889e435fd642bc18d0cb8a2a77608f2e1b767
Radoslaw <rdkgsk@gmail.com>
2026-03-01 17:45:24 +0100

    Refactor project structure
    Refactor structure to facilitate multiple projects and libraries

---
+++ src/cad_math/include/cad_math/math.h
@@ -0,0 +1,8 @@
+//
+// Created by rdkgsk on 3/1/26.
+//
+
+#ifndef CAD_MATH_H
+#define CAD_MATH_H
+
+#endif //CAD_MATH_H
---
+++ src/cad_math/src/math.cpp
@@ -0,0 +1,6 @@
+//
+// Created by rdkgsk on 3/1/26.
+//
+
+#include <cad_math/math.h>
+
