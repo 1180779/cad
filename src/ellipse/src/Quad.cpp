00f93d3b9b4aea80a46414344b4ac6f0569cae97
1180779 <rdkgsk@gmail.com>
2026-07-06 14:23:09 +0200

    Fix gl calls in ellipse

--- src/ellipse/src/Quad.cpp
+++ src/ellipse/src/Quad.cpp
@@ -30,7 +30,7 @@ Quad::Quad() : m_vao(0), m_vbo(0) {
         0.0f
     };
 
-    const auto gl = gl();
+    const auto gl = getGl();
     gl->glGenVertexArrays(1, &m_vao);
     gl->glGenBuffers(1, &m_vbo);
 
@@ -48,13 +48,13 @@ Quad::Quad() : m_vao(0), m_vbo(0) {
 }
 
 Quad::~Quad() {
-    const auto gl = gl();
+    const auto gl = getGl();
     gl->glDeleteVertexArrays(1, &m_vao);
     gl->glDeleteBuffers(1, &m_vbo);
 }
 
 void Quad::draw() const {
-    const auto gl = gl();
+    const auto gl = getGl();
     gl->glBindVertexArray(m_vao);
     gl->glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     gl->glBindVertexArray(0);
